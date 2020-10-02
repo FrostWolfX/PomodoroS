5cfff7b35647561817f62cd78e3d27540b98c4d3
FrostWolfX <stukalov.blog@gmail.com>
2020-10-02 23:52:46 +0300

    time now + interval
    Суммирую время, первоначальный интерфейс
    new Timer
    
    new class Timer tick

---
+++ PomodoroS/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading.Tasks;
+
+namespace PomodoroS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int timeMinute;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timeMinute = Convert.ToInt32(SettingPomodoroTime.Text);
+        }
+
+
+
+        private void SaveSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void PomodoroStart_Click(object sender, RoutedEventArgs e)
+        {
+            SmallTimer smallTimer = new SmallTimer(timeMinute);
+        }
+    }
+}
---
+++ PomodoroS/SmallTimer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PomodoroS
+{
+    class SmallTimer
+    {
+        private int Minutes; 
+
+        public SmallTimer(int minutes)
+        {
+            Minutes = minutes;
+            Timer();
+        }
+
+        public void Timer()
+        {
+            // устанавливаем метод обратного вызова
+            TimerCallback tm = new TimerCallback(Count);
+            // создаем таймер
+            Timer timer = new Timer(tm, Minutes, 0, 1000);
+        }
+        public void Count(object obj)
+        {
+            int x = (int)obj;
+        }
+    }
+}
